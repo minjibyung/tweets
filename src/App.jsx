45cbc957ad94ceaddf5021f965de79cb3599105a
minjibyung <minjibyung@gmail.com>
2023-03-04 22:13:12 -0500

    se agregan cambios x3

--- src/App.jsx
+++ src/App.jsx
@@ -7,66 +7,68 @@ let tweets = [];
 function App() {
 
 const TA = useRef();
-const [message, setMessage] = useState("Aquí verás tu Tweet actual")
+const [message, setMessage] = useState("Aquí verás tu Tweet actual");
 const [counter, setCounter] = useState(255);
 const [styleCounter, setStyleCounter] = useState("counter");
 const [tweetList, setTweetList] = useState([]);
 
 const getMessage = () => {
-setMessage(TA.current.value)
+setMessage(TA.current.value);
 setCounter(255 - TA.current.value.length);
-// if (counter <= 0) {
-// setStyleCounter("counter-red");
-// } else {
-// setStyleCounter("counter");
-// }
-}
+if (TA.current.value.length > 255) {
+    setStyleCounter("counter-limit");
+    alert("solo son 255 caracteres")
+  } else {
+    setStyleCounter("counter");
+  }
+};
+
 
 const handleSendTweet = () => {
 const newTweet = { message };
 tweets.push(newTweet);
 setTweetList([...tweetList, newTweet]);
-setMessage("Tweet publicado");
-setCounter(0);
+setMessage("");
+setCounter(255);
 TA.current.value = '';
+};
+
+const handleArchiveTweet = (tweets) => {
+const updatedTweets = tweetList.filter((t) => t !== tweets);
+setTweetList(updatedTweets);
+tweets = updatedTweets;
 }
 
-const handleArchiveTweet = (tweet) => {
-const index = tweetList.indexOf(tweet);
-if (index !== -1) {
-tweets.splice(index, 1);
-setTweetList(tweets);
-}
-}
 
-return (
-<div className="App">
-<h1>Generador De Tweets</h1>
-<ContainerTweet
-contain = {
-<Tweet
-eventTweet={handleSendTweet}
-refTextArea={TA}
-eventArea={message}
-eventFile={handleArchiveTweet}
-eventList={() => setTweetList(tweets)}
-eventMessage={getMessage}
-counter={counter}
-styleCounter={styleCounter}
-/>}
+ return (
+   <div className="App">
+    <h1>Generador De Tweets</h1>
+    <ContainerTweet
+     contain = {
+     <Tweet
+       eventTweet={handleSendTweet}
+       refTextArea={TA}
+       eventArea={message}
+       eventFile={handleArchiveTweet}
+       eventList={() => setTweetList(tweets)}
+       eventMessage={getMessage}
+       counter={counter}
+       styleCounter={styleCounter}
 />
-<h3>Aquí se verán tus tweets archivados</h3>
-{tweetList.length > 0 ? (
-tweetList.map((tweet) => (
-<div key={tweet.message}>
-<p>{tweet.message}</p>
-</div>
+}
+/>
+   <h3>Aquí se verán tus tweets archivados</h3>
+   {tweetList.length > 0 ? (
+    tweetList.map((tweets) => (
+      <div key={tweets.message}>
+       <p>{tweets.message}</p>
+  </div>
 ))
 ) : (
-<p>No hay tweets archivados</p>
+    <p>No hay tweets archivados</p>
 )}
-</div>
-)
+  </div>
+  )
 }
 
 export default App
